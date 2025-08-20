86c546395b418df99f54e552bdc2b80a6ae273ad
jarivatoi <81554724+jarivatoi@users.noreply.github.com>
2025-08-20 14:51:50 +0400

    Updated ScrollingTabs.tsx

--- src/components/credit/ScrollingTabs.tsx
+++ src/components/credit/ScrollingTabs.tsx
@@ -58,8 +58,8 @@ const ScrollingTabs: React.FC<ScrollingTabsProps> = ({
     container.offsetWidth;
     content.offsetWidth;
     
-    const containerWidth =  container.offsetWidth-400;
-    const contentWidth = content.scrollWidth-400;
+    const containerWidth =  container.offsetWidth;
+    const contentWidth = content.scrollWidth;
     
     // Calculate duration based on content width
     const pixelsPerSecond = 80; // Increase for faster scrolling, decrease for slower
@@ -173,8 +173,8 @@ const ScrollingTabs: React.FC<ScrollingTabsProps> = ({
       container.offsetWidth;
       content.offsetWidth;
       
-      const containerWidth = container.offsetWidth-400;
-      const contentWidth = content.scrollWidth-400;
+      const containerWidth = container.offsetWidth;
+      const contentWidth = content.scrollWidth;
       
       // Calculate duration based on content width
       const pixelsPerSecond = 80; // Keep consistent with getAnimationParams
