1dbf7a5eeb49ef7ad27cd9470242081b84cb20f0
jarivatoi <81554724+jarivatoi@users.noreply.github.com>
2025-08-20 13:52:15 +0400

    Updated ScrollingTabs.tsx

--- src/components/credit/ScrollingTabs.tsx
+++ src/components/credit/ScrollingTabs.tsx
@@ -58,12 +58,12 @@ const ScrollingTabs: React.FC<ScrollingTabsProps> = ({
     container.offsetWidth;
     content.offsetWidth;
     
-    const containerWidth = container.offsetWidth-500;
-    const contentWidth = content.scrollWidth-500;
+    const containerWidth = container.offsetWidth-400;
+    const contentWidth = content.scrollWidth-400;
     
     // Calculate duration based on content width
     const pixelsPerSecond = 20;
-    const totalDistance = contentWidth + containerWidth-1000;
+    const totalDistance = contentWidth + containerWidth-800;
     const duration = totalDistance / pixelsPerSecond;
     
     return { containerWidth, contentWidth, pixelsPerSecond, totalDistance, duration };
