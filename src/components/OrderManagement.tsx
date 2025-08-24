f981440dc194eff23fcb843b24f7788c3c777b9e
jarivatoi <81554724+jarivatoi@users.noreply.github.com>
2025-08-24 08:36:24 +0400

    Remove VAT percentage input field

--- src/components/OrderManagement.tsx
+++ src/components/OrderManagement.tsx
@@ -306,22 +306,6 @@ const OrderManagement: React.FC = () => {
                   </button>
                 </div>
               </form>
-            )}
-
-            {/* Search Bar */}
-            <div className="mt-4 select-none">
-              <div className="relative w-full max-w-md mx-auto select-none">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <Search size={20} className="text-gray-400" />
-                </div>
-                <input
-                  type="text"
-                  value={searchQuery}
-                  onChange={(e) => setSearchQuery(e.target.value)}
-                  placeholder="Search categories..."
-                  className="block w-full pl-10 pr-4 py-3 text-lg border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-purple-500 select-text"
-                />
-              </div>
             </div>
           </>
         ) : (
